d3eb64aa759301c64a1031769284736a30adb384
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-05 02:10:37 +0800

    calculate and add-subtract donate amounts

--- js/index.js
+++ js/index.js
@@ -13,13 +13,22 @@ window.addEventListener('scroll', function() {
   }
 })
 
-// document.getElementById('button-donate-1').addEventListener('click', function() {
-//   isValidAmount('donate-input-1', 'my_modal_1');
-// });
+document.getElementById('button-donate-1').addEventListener('click', function() {
+  isValidAmount('donate-input-1', 'my_modal_1', 'noakhali-donated-amount');
+});
+
+document.getElementById('button-donate-2').addEventListener('click', function() {
+  isValidAmount('donate-input-2', 'my_modal_2', 'feni-donated-amount');
+});
+
+document.getElementById('button-donate-3').addEventListener('click', function() {
+  isValidAmount('donate-input-3', 'my_modal_3', 'quota-donated-amount');
+})
 
 // History
 const historyItem = document.createElement('div');
 historyItem.className = "border border-gray-300 rounded-lg p-8 mb-5";
 historyItem.innerHTML = `
-  <p></p>
+  <p class="text-xl font-bold">96500 Taka is Donated for famine-2024 at Feni, Bangladesh</p>
+  <p class="text-sm font-light">Date: ${new Date().toLocaleString()}</p>
 `
--- js/utilities.js
+++ js/utilities.js
@@ -1,4 +1,4 @@
-function isValidAmount(input, modal) {
+function isValidAmount(input, modal, donateAmount) {
   const inputAmount = document.getElementById(input).value;
   const amount = parseFloat(inputAmount);
 
@@ -6,10 +6,36 @@ function isValidAmount(input, modal) {
     alert('Invalid Donation Amount');
     return;
   }
-  else {
-    const modalId = document.getElementById(modal);
-    modalId.showModal();
+  
+  const balanceElement = document.getElementById('current-balance');
+  const currentBalance = parseFloat(balanceElement.innerText);
+
+  if(amount > currentBalance) {
+    alert('Insufficient Balance');
+    return;
   }
+  
+  const newBalance = currentBalance - amount;
+  balanceElement.innerText = newBalance.toFixed(2);
+
+  const donateElement = document.getElementById(donateAmount);
+  const previousDonated = parseFloat(donateElement.innerText);
+  const newDonated = previousDonated + amount;
+  donateElement.innerText = newDonated.toFixed(2);
+
+  const modalId = document.getElementById(modal);
+  modalId.showModal();
+
+
+  // else {
+  //   const modalId = document.getElementById(modal);
+  //   const currentBalance = document.getElementById('current-balance');
+  //   const newBalance = currentBalance - donateAmount;
+  //   currentBalance.innerText = newBalance.toFixed(2);
+  //   const donateTotal = amount + donateAmount;
+  //   document.getElementById('noakhali-donated-amount').innerText = donateTotal.toFixed(2);
+  //   modalId.showModal();
+  // }
 }
 
 // making the isValidAmount function global for reusable code
